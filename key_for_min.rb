f81f878940f43eeba6108d122b728f60663c4a1f
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-08 18:45:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,5 @@ def key_for_min_value(name_hash)
   min_value = nil
   name_hash.each do |key, value|
     if min_value == nil || value < min_value
-      min_value == value
+      min_value = value
 end
